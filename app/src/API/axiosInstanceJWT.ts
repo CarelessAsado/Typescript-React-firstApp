d20fc9d1f699e6f06ed14d5738508426df28ac76
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-02-18 13:01:32 -0300

    backend heroku conectado a Git

--- app/src/API/axiosInstanceJWT.ts
+++ app/src/API/axiosInstanceJWT.ts
@@ -1,5 +1,6 @@
 const axios = require("axios").default;
-export const baseUrl = "https://ts-firstapp.herokuapp.com/api/v1";
+export const baseUrl =
+  "https://typescript-backend-reactnode.herokuapp.com/api/v1";
 export default axios.create({
   baseURL: baseUrl,
   headers: {
