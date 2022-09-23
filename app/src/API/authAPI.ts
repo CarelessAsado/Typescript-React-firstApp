daf10074ef9753367122152e5002532b6c2082d3
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-09-23 14:19:46 -0300

    login axios instance changed

--- app/src/API/authAPI.ts
+++ app/src/API/authAPI.ts
@@ -2,40 +2,8 @@ import loginAxiosInstance from "./loginAxiosInstance";
 import { BACKEND_URL } from "config/constants";
 import axiosInstanceJWT from "./axiosInstanceJWT";
 export const authAPI = {
-  /*-------------------------LOGOUT-----------------------------*/
-  /* ver si envio la cookie asi la borro en el backend */
-  /*-------------------------LOGIN-----------------------------*/
-  /*   login: async function (
-    e: React.FormEvent<HTMLFormElement>,
-    loginInput: ILoginInput,
-    dispatch: React.Dispatch<Actions>,
-    navigate: NavigateFunction,
-    errorAssert: React.MutableRefObject<HTMLDivElement>
-  ): Promise<void> {
-    e.preventDefault();
-    dispatch({ type: ActionsEnum.START_FETCH_ALL });
-    try {
-      // mando los headers ya directamente del backend 
-      const { data, headers } = await loginAxiosInstance.post(
-        "/auth/login",
-        loginInput
-      );
-      const loggedUser = { ...data, token: headers["auth-token"] };
-      dispatch({
-        type: ActionsEnum.SUCCESS_LOGIN,
-        payload: loggedUser,
-      });
-      setHeaders(loggedUser.token);
-      localStorage.setItem("user", JSON.stringify(loggedUser));
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-       this.renderError(dispatch, error); 
-      errorAssert?.current?.focus();
-    }
-  }, */
   login: function (loginInput: ILoginInput) {
-    return axiosInstanceJWT.post<LoginSuccessful>(
+    return loginAxiosInstance.post<LoginSuccessful>(
       BACKEND_URL.login(),
       loginInput
     );
